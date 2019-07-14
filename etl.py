90d9d423cbafd4eab39fe14b6da8bfa1f75e02b1
vodopianova <oleksandra.jmail@gmail.com>
2019-07-14 16:42:48 -0700

    project 1

--- etl.py
+++ etl.py
@@ -3,41 +3,46 @@ import glob
 import psycopg2
 import pandas as pd
 from sql_queries import *
-
+import datetime
 
 def process_song_file(cur, filepath):
     # open song file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # insert song record
-    song_data = 
+    song_data = df[['song_id', 'title','artist_id', 'year', 'duration']].values[0].tolist()
     cur.execute(song_table_insert, song_data)
     
     # insert artist record
-    artist_data = 
+    artist_data =     artist_data = df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].values[0].tolist()
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df[df['page']=="NextSong"].reset_index()
 
     # convert timestamp column to datetime
-    t = 
+    t = pd.to_datetime(df.ts, unit='ms')
     
-    # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    # get week_day and week
+    df['week'] = t.apply(lambda x: datetime.date(x.year, x.month, x.day).isocalendar()[1])
+    df['week_day'] = t.apply(lambda x: datetime.date(x.year, x.month, x.day).strftime("%A"))
 
+    # insert time data records
+    time_data = (t, t.dt.hour, t.dt.day, df.week, t.dt.month, t.dt.year, df.week_day)
+    column_labels = ['start_time','hour','day','week','month', 'year','weekday']
+    time_df = pd.DataFrame(dict(zip(column_labels, time_data))) 
+    df['start_time'] = t
+    
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df[['userId','firstName', 'lastName', 'gender', 'level']]
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -56,7 +61,7 @@ def process_log_file(cur, filepath):
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
+        songplay_data = (row.start_time,row.userId,row.level,songid,artistid, row.sessionId,row.location,row.userAgent)
         cur.execute(songplay_table_insert, songplay_data)
 
 
@@ -91,3 +96,20 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
